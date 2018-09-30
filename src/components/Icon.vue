5af3bd83f0163d7f22931e85fbf8d495be4a20ab
Dustin Delatore <dustin@delatore.me>
2018-09-30 00:55:45 -0400

    Add Icon sprite

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <svg class='icon' v-bind='$props'>
+    <use v-bind="{'xlink:href':'/feather-sprite.svg#'+name}"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: String,
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.icon
+  stroke currentColor
+  stroke-width 2
+  stroke-linecap round
+  stroke-linejoin round
+  fill none
+</style>
