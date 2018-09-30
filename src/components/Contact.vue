fcb61b81ee7dc1c5443f81bdddae7aa50a54754c
Dustin Delatore <dustin@delatore.me>
2018-09-30 00:57:04 -0400

    Update style to contact card

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,26 +1,44 @@
 <template lang="html">
-  <div class='contact-card'
+  <div class="contact-card"
     @mouseover='showActions = true'
     @mouseleave='showActions = false'>
 
-    <template v-if="!showEdit">
-      <h2 class="title">{{ fullName }}</h2>
-      <p><b>Email:</b> {{ contact.email }}</p>
-    </template>
+    <div class="body" v-if="!showEdit">
+      <div class="media" :style="{backgroundColor: bgColor}">
+        {{ initials }}
+      </div>
+      <div class="content">
+        <h4 class="title">{{ fullName }}</h4>
+        <p class="meta"><b>Email:</b> {{ contact.email }}</p>
+      </div>
+
+      <div class="actions" :class="{ show: showActions }">
+        <button
+          class="button -icon-center -fill-gray -small"
+          @click="toggleForm"
+          :title="editText">
+          <span class="sr-only">{{ editText }}</span>
+          <Icon :name="editText + '-2'" />
+        </button>
+        <button
+          class="button -icon-center -fill-error -small"
+          @click="removeContact"
+          title="Delete">
+          <Icon name="trash-2" />
+        </button>
+      </div>
+    </div>
 
     <ContactForm v-else edit
-      :contact='contact'
-      @toggle='toggleForm' />
+      :contact="contact"
+      @toggle="toggleForm" />
 
-    <div class="actions" :class="{ show: showActions }">
-      <button class="button -fill-error -small" @click="removeContact">Delete</button>
-      <button class="button -fill-gray -small" @click="toggleForm">{{ editText }}</button>
-    </div>
   </div>
 </template>
 
 <script>
 import ContactForm from '@/components/ContactForm.vue'
+import randomColor from 'randomcolor'
 
 export default {
   name: 'contact-item',
@@ -31,18 +49,32 @@ export default {
   data() {
     return {
       showEdit: false,
-      editText: 'Edit',
-      showActions: false
+      editText: 'edit',
+      showActions: false,
+      bgColor: randomColor({luminosity: 'light'})
     }
   },
   computed: {
     fullName() {
       return `${this.contact.fname} ${this.contact.lname}`
+    },
+    initials() {
+      const name = [
+        this.contact.fname,
+        this.contact.lname
+      ]
+
+      let initials = ""
+      name.forEach(str => {
+        initials += str.charAt(0)
+      })
+
+      return initials.toUpperCase()
     }
   },
   methods: {
     toggleForm() {
-      this.editText = this.showEdit ? 'Edit' : 'Save'
+      this.editText = this.showEdit ? 'edit' : 'save'
       this.showEdit = !this.showEdit
     },
     removeContact() {
@@ -52,25 +84,53 @@ export default {
 }
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
 .contact-card
   padding 20px
   margin-bottom 24px
   transition all 0.2s linear
   border-radius 6px
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19)
-  cursor: pointer
+  box-shadow 0 6px 22px 0 rgba(0,0,0,0.15)
+  background #f5f5f5
+  cursor pointer
 
-.contact-card > .title
+.media
+  display flex
+  align-items center
+  justify-content center
+  width 50px
+  height 50px
+  font-weight bold
+  border-radius 25px
+  background tomato
+  color #000
+
+.content
+  flex-grow 1
+  padding 0 10px
+
+.title
+.meta
   margin 0
 
-.contact-card > p:first-of-type
-  margin-top 0
+.body
+  display flex
+  justify-content space-between
+  align-items center
 
 .actions
+  display flex
+  flex-flow column
+  flex-basis 0
   opacity 0.4
   transition opacity 0.3s ease-in-out
 
+  .button
+    margin 3px 0
+
   &.show
     opacity 1
+
+  @media (min-width: 600px)
+    flex-flow row
 </style>
